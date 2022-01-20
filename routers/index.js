19b528298e9b24ed4bb7cf3d614bd25d0b9a34aa
chicodevbr <chicodevbr@gmail.com>
2022-01-20 09:54:22 -0300

    refatorando rota index

--- routers/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  console.log(`API DEVinBank version 1.0`);
-  res.send('Welcome to API DEVinBank - version 1.0');
-});
-
-module.exports = router;

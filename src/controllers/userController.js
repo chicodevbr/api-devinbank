f4188529dd3b7766971dd0dbc8dfd45d662b6db6
chicodevbr <chicodevbr@gmail.com>
2022-01-21 08:23:57 -0300

    criando endpoints getuser e getuserbyid

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,21 @@
+const { getData, createOrUpdateData } = require('../utils/functions');
+const userService = require('../services/userService');
+
 module.exports = {
-    async index(req, res){
-        const users 
+  async index(req, res) {
+    const users = getData('user.json');
+
+    return res.status(200).json({ users: users });
+  },
+
+  async indexOne(req, res) {
+    const { id } = req.params;
+    try {
+      const response = await userService.getUserById(id);
+      return res.status(400).json(response);
+    } catch (error) {
+      console.log(error.message);
+      return res.status(400).json({ error: error.message });
     }
-}
+  },
+};

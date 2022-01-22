1a79fd7f5b6876e77896011d99960ffdc575a989
chicodevbr <chicodevbr@gmail.com>
2022-01-22 09:22:10 -0300

    adicionando documentação ao swagger

--- src/swagger.js
+++ src/swagger.js
@@ -1,8 +1,9 @@
 const swaggerAutogen = require('swagger-autogen')();
 const outputFile = './src/swagger_output.json';
+const doc = require('../src/utils/doc');
 
 const endpointFiles = ['./src/app.js'];
 
-swaggerAutogen(outputFile, endpointFiles).then(() => {
+swaggerAutogen(outputFile, endpointFiles, doc).then(() => {
   require('./app.js');
 });

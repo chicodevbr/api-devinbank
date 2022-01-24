09dc0f16b34e2a490da20769075735d245d1ffb9
chicodevbr <chicodevbr@gmail.com>
2022-01-24 08:40:40 -0300

    criando service para devolver expenses por users

--- src/services/financial.js
+++ src/services/financial.js
@@ -8,7 +8,16 @@ const getExpensesByUserId = async (userId) => {
   const data = await getData('financial.json');
   return await data.filter((item) => item.userId === userId);
 };
+
+const getExpensesByUserAndQuery = async (userId, query) => {
+  const expensesData = await getExpensesByUserId(userId);
+  console.log(expensesData[1].financialData);
+  return await expensesData[0].financialData.filter(
+    (item) => item.typeOfExpenses === query
+  );
+};
 module.exports = {
   getAllExpenses,
   getExpensesByUserId,
+  getExpensesByUserAndQuery,
 };

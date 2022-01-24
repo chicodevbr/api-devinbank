3875a3f79eb898ddf4b920f81cfc2c86af957ab3
chicodevbr <chicodevbr@gmail.com>
2022-01-24 11:28:21 -0300

    criando service para devolver expenses por userId

--- src/services/financial.js
+++ src/services/financial.js
@@ -1,20 +1,34 @@
 const { getData, createOrUpdateData } = require('../utils/functions');
 
 const getAllExpenses = async () => {
-  return await getData('financial.json');
+  return await getData('financial');
 };
 
 const getExpensesByUserId = async (userId) => {
-  const data = await getData('financial.json');
+  const data = await getData('financial');
   return await data.filter((item) => item.userId === userId);
 };
 
 const getExpensesByUserAndQuery = async (userId, query) => {
   const expensesData = await getExpensesByUserId(userId);
-  console.log(expensesData[1].financialData);
-  return await expensesData[0].financialData.filter(
-    (item) => item.typeOfExpenses === query
-  );
+  const expensesFilteredByQuery = expensesData.financialData((item) => {
+    const filteredByQuery = Object.keys(query).filter((key) => {
+      return (
+        query[key] &&
+        item[key] &&
+        String(
+          item[key]
+            .toLocaleLowerCase()
+            .includes(String(query[key].toLocaleLowerCase()))
+        )
+      );
+    });
+    return filteredByQuery.length > 0;
+  });
+
+  return expensesFilteredByQuery.length > 0
+    ? expensesFilteredByQuery
+    : expensesData.financialData;
 };
 module.exports = {
   getAllExpenses,
